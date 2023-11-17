d86c101128f372e2fdc001c9a741472a1403e6bf
tubosuncool <94539967+tubosuncool@users.noreply.github.com>
2023-11-17 20:04:48 +0800

    更新 v2freeCheckIn.js

--- v2freeCheckIn.js
+++ v2freeCheckIn.js
@@ -1,4 +1,5 @@
 # v2free_cookie获取 @zqzess
 hostname = v2free.org
 
-^https:\/\/v2free\.org\/user url script-request-header https://raw.githubusercontent.com/zqzess/rule_for_quantumultX/master/js/Mine/v2freeCheckIn/v2freeCheckIn.js
+^https:\/\/v2free\.org\/user url script-request-header
+https://raw.githubusercontent.com/tubosuncool/v2freeCheckIn_cookie/main/v2freeCheckIn.js
