2b325239ced0bac23fa1bb36641e6d1bcfd53c90
tubosuncool <94539967+tubosuncool@users.noreply.github.com>
2023-11-17 17:27:13 +0800

    Create v2freeCheckIn.js

---
+++ v2freeCheckIn.js
@@ -0,0 +1,4 @@
+# v2free_cookie获取 @zqzess
+hostname = v2free.org
+
+^https:\/\/v2free\.org\/user url script-request-header https://raw.githubusercontent.com/zqzess/rule_for_quantumultX/master/js/Mine/v2freeCheckIn/v2freeCheckIn.js
